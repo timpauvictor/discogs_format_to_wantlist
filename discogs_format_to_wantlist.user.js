4bf972128bf19da0b37fa2cf0c116862aa00fa9e
minghealemigliori <minghe.alemigliori@gmail.com>
2016-08-18 19:31:25 +0200

    Update discogs_format_to_wantlist.user.js

--- discogs_format_to_wantlist.user.js
+++ discogs_format_to_wantlist.user.js
@@ -28,22 +28,22 @@ countries = [
   'Finland',
   'Netherlands',
   'Italy',
-  'Czech Republic'
-  'Portugal'
-  'New Zealand'
-  'Turkey'
-  'South Africa'
-  'Brazil'
-  'Venezuela'
-  'Japan'
-  'Argentina'
-  'Israel'
-  'Mexico'
-  'Chile'
-  'Denmark'
-  'Peru'
-  'Colombia'
-  'Taiwan'
+  'Czech Republic',
+  'Portugal',
+  'New Zealand',
+  'Turkey',
+  'South Africa',
+  'Brazil',
+  'Venezuela',
+  'Japan',
+  'Argentina',
+  'Israel',
+  'Mexico',
+  'Chile',
+  'Denmark',
+  'Peru',
+  'Colombia',
+  'Taiwan',
 ];
 function Add_Format($albumformat)
 {
