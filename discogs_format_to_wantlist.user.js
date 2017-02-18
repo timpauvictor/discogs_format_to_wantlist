ad168451e836e0b2b58aa68de18e365300d6c8dd
minghealemigliori <minghe.alemigliori@gmail.com>
2017-02-18 13:33:28 +0100

    Update discogs_format_to_wantlist.user.js

--- discogs_format_to_wantlist.user.js
+++ discogs_format_to_wantlist.user.js
@@ -2,7 +2,7 @@
 // @require http://code.jquery.com/jquery-latest.js
 // @name         Add all format to wantlist
 // @namespace    http://dollardialup.com/
-// @version      0.7
+// @version      0.8
 // @description  adds buttons to discogs master release page for adding all of a certain format to your wantlist
 // @author       Alessandro Minghe Migliori, Joey Liechty, Scott Powers
 // @match        https://www.discogs.com/master/*
